71bfebd1dd0ad3bcc14d0c7e5c3715bdfcfec0d3
EVolkogon <e.volgon@gmail.com>
2016-01-01 10:14:14 +0200

    Work DEV version. Manual control only.

--- config.py
+++ config.py
@@ -1,13 +1,22 @@
+"""
+This is module with configuration. Latter it be rewrite to JSON format.
+Use absolute path ONLY!
+"""
          #Config Name   Config Unit
-         
+
+# dynamic path for test ONLY!
+import os
+script_path = os.path.dirname(os.path.realpath(__file__))
+
 CONFIG = {  "DATA":     # Profile
                         { "LONERS": #Path
-                                    ["test/DATA/save_me.txt",
-                                     "test/DATA/folder_for_save",
+                                    [script_path + "/test/DATA/save_me.txt",
+                                     script_path +"/test/DATA/folder_for_save",
                                       ],
                          "Project" : ["./"],
                         },
             "STORAGES":
-                        {"STOR_1" :"../test/DIR_FOR_BKP",
+                        {"STOR_1" :script_path +"/test/DIR_FOR_BKP",
                          },
          }
+print CONFIG["DATA"]["LONERS"][0]
